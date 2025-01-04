5c3d69fa7dd7ab2301bbd394773247dc9e620e18
Florent <florent.guyard@live.fr>
2025-01-04 11:14:16 +0100

    simplification code sauvegarde

--- src/main/java/com/alibou/websocket/chat/Repository/MessageRepository.java
+++ src/main/java/com/alibou/websocket/chat/Repository/MessageRepository.java
@@ -3,9 +3,11 @@ package com.alibou.websocket.chat.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.alibou.websocket.chat.Models.ChatMessage;
 
+@Repository
 public interface MessageRepository extends JpaRepository<ChatMessage, Long> {
     List<ChatMessage> findByConversationIdOrderByDateAscTimeAsc(String conversationId);
 }
